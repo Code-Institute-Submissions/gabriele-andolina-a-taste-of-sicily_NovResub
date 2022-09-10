7d87ce316f1fe642c35b0c6084ef82d0d1ad1b83
Gabriele Andolina <gabe.andolina@gmail.com>
2022-09-10 09:34:30 +0000

    Add product models and fixtures

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category, Wine, Food, ExperienceType
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Wine)
+admin.site.register(Food)
+admin.site.register(ExperienceType)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    friendly_name = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Wine(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=255)
+    image = models.ImageField(null=True, blank=True)
+    description = models.TextField()
+    grape_variety = models.CharField(max_length=255, null=True, blank=True)
+    vintage = models.CharField(max_length=255, null=True, blank=True)
+    certification = models.CharField(max_length=255, null=True, blank=True)
+    colour = models.CharField(max_length=255, null=True, blank=True)
+    type = models.CharField(max_length=255, null=True, blank=True)
+    alcohol_content = models.CharField(max_length=255, null=True, blank=True)
+    format = models.CharField(max_length=255, null=True, blank=True)
+    place_of_origin = models.CharField(max_length=255, null=True, blank=True)
+    vegan_ok = models.ImageField(null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Food(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    type = models.CharField(max_length=255, null=True, blank=True)
+    name = models.CharField(max_length=255)
+    image = models.ImageField(null=True, blank=True)
+    description = models.TextField()
+    ingredients = models.CharField(max_length=255, null=True, blank=True)
+    certification = models.CharField(max_length=255, null=True, blank=True)
+    format = models.CharField(max_length=255, null=True, blank=True)
+    place_of_origin = models.CharField(max_length=255, null=True, blank=True)
+    vegan_ok = models.ImageField(null=True, blank=True)
+    shelf_life = models.CharField(max_length=255, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class ExperienceType(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    type = models.CharField(max_length=255, null=True, blank=True)
+    name = models.CharField(max_length=255)
+    image = models.ImageField(null=True, blank=True)
+    description = models.TextField()
+    duration = models.CharField(max_length=255, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
