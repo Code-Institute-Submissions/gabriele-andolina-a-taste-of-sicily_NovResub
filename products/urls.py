c475922b3358c10f0bc34a9904f4f0ff8fa29ecd
Gabriele Andolina <gabe.andolina@gmail.com>
2022-09-11 15:34:29 +0000

    Add complete functionality to view wine details

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_products, name='all_products')
+    path('', views.view_products, name='all_products'),
+    path('<wine_id>', views.view_wine_details, name='wine_details'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,21 +1,35 @@
 from itertools import chain
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Wine, Food
 
 
 def view_products(request):
     """
     A view to display the shop's products and to handle
-    sorting and search queries
+    sorting and search queries.
     """
 
     wines = Wine.objects.all()
     foods = Food.objects.all()
     all_products = list(chain(wines, foods))
-    print(all_products)
 
     context = {
         'products': all_products
     }
 
     return render(request, 'products/products.html', context)
+
+
+def view_wine_details(request, wine_id):
+    """
+    A view to display full details for each wine.
+    """
+
+    wine = get_object_or_404(Wine, pk=wine_id)
+
+    context = {
+        'wine': wine
+    }
+
+    return render(request, 'products/wine_details.html', context)
+
