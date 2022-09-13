2f8ad8b5c09260f542a421ad051a7ea72f39e987
Gabriele Andolina <gabe.andolina@gmail.com>
2022-09-13 19:07:40 +0000

    Add add_wine to cart functionality

--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_cart(request):
     """ A view to display the shopping cart. """
 
     return render(request, 'cart/cart.html')
+
+
+def add_wine(request, item_id):
+    """ A view to add a quantity of the selected wine to the shopping cart. """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    return redirect(redirect_url)
