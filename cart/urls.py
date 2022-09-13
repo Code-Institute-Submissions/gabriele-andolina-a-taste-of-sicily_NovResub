759490f8fbc2a93d9f8e5d21a098993e717de180
Gabriele Andolina <gabe.andolina@gmail.com>
2022-09-13 19:30:54 +0000

    Add add_food to cart functionality

--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.view_cart, name='view_cart'),
-    path('add/<item_id>/', views.add_wine, name='add_wine'),
+    path('add/<wine_id>/', views.add_wine, name='add_wine'),
+    path('add/<food_id>/', views.add_food, name='add_food'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -7,17 +7,35 @@ def view_cart(request):
     return render(request, 'cart/cart.html')
 
 
-def add_wine(request, item_id):
+def add_wine(request, wine_id):
     """ A view to add a quantity of the selected wine to the shopping cart. """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
-    if item_id in list(cart.keys()):
-        cart[item_id] += quantity
+    if wine_id in list(cart.keys()):
+        cart[wine_id] += quantity
     else:
-        cart[item_id] = quantity
+        cart[wine_id] = quantity
 
     request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)
+
+
+def add_food(request, food_id):
+    """ A view to add a quantity of the selected wine to the shopping cart. """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if food_id in list(cart.keys()):
+        cart[food_id] += quantity
+    else:
+        cart[food_id] = quantity
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
     return redirect(redirect_url)
