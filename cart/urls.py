2051a7b0e41945983dec1d934656fef4569bc18e
Gabriele Andolina <gabe.andolina@gmail.com>
2022-09-24 21:56:30 +0000

    Add views to adjust and remove items from bag and calc_subtotal template filter

--- cart/urls.py
+++ cart/urls.py
@@ -5,4 +5,10 @@ urlpatterns = [
     path('', views.view_cart, name='view_cart'),
     path('add/<int:wine_id>/', views.add_wine, name='add_wine'),
     path('add/<int:food_id>/', views.add_food, name='add_food'),
+    path('adjust/<int:wine_id>/', views.adjust_wine_quantity,
+         name='adjust_wine_qty'),
+    path('adjust/<int:food_id>/', views.adjust_food_quantity,
+         name='adjust_food_qty'),
+    path('remove/<int:wine_id>/', views.remove_wine, name='remove_wine'),
+    path('remove/<int:food_id>/', views.remove_food, name='remove_food'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_cart(request):
@@ -24,7 +24,7 @@ def add_wine(request, wine_id):
 
 
 def add_food(request, food_id):
-    """ A view to add a quantity of the selected wine to the shopping cart. """
+    """ A view to add a quantity of the selected food to the shopping cart. """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -37,3 +37,63 @@ def add_food(request, food_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def adjust_wine_quantity(request, wine_id):
+    """
+    A view to adjust the quantity of the selected wine
+    within the shopping cart.
+    """
+
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[wine_id] = quantity
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
+def adjust_food_quantity(request, food_id):
+    """
+    A view to adjust the quantity of the selected food
+    within the shopping cart.
+    """
+
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[food_id] = quantity
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
+def remove_wine(request, wine_id):
+    """ A view to remove the selected wine from the shopping cart. """
+
+    try:
+        cart = request.session.get('cart', {})
+
+        cart.pop(wine_id)
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
+
+
+def remove_food(request, food_id):
+    """ A view to remove the selected food from the shopping cart. """
+
+    try:
+        cart = request.session.get('cart', {})
+
+        cart.pop(food_id)
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
