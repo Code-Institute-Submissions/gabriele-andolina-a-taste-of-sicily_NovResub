c71b7a08642f2006d4da2184e3967f8aadb70e9c
Gabriele Andolina <gabe.andolina@gmail.com>
2022-09-25 11:19:07 +0000

    Update wine views in cart to include specific messages

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 from products.models import Wine, Food
 
@@ -11,13 +11,14 @@ def view_cart(request):
 def add_wine(request, wine_id):
     """ A view to add a quantity of the selected wine to the shopping cart. """
 
-    wine = Wine.objects.get(pk=wine_id)
+    wine = get_object_or_404(Wine, pk=wine_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if wine_id in list(cart.keys()):
         cart[wine_id] += quantity
+        messages.success(request, f'Updated {wine.name} quantity to {cart[wine_id]}.')
     else:
         cart[wine_id] = quantity
         messages.success(request, f'Added {wine.name} to the cart. Cheers!')
@@ -48,11 +49,13 @@ def adjust_wine_quantity(request, wine_id):
     within the shopping cart.
     """
 
+    wine = get_object_or_404(Wine, pk=wine_id)
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
     if quantity > 0:
         cart[wine_id] = quantity
+        messages.success(request, f'Updated {wine.name} quantity to {cart[wine_id]}.')
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
@@ -78,13 +81,15 @@ def remove_wine(request, wine_id):
     """ A view to remove the selected wine from the shopping cart. """
 
     try:
+        wine = get_object_or_404(Wine, pk=wine_id)
         cart = request.session.get('cart', {})
-
         cart.pop(wine_id)
+        messages.success(request, f'Removed {wine.name} from your cart.')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
     except Exception as e:
+        messages.error(request, f'Oops! An error occurred while removing the item: {e}')
         return HttpResponse(status=500)
 
 
@@ -93,7 +98,6 @@ def remove_food(request, food_id):
 
     try:
         cart = request.session.get('cart', {})
-
         cart.pop(food_id)
 
         request.session['cart'] = cart
