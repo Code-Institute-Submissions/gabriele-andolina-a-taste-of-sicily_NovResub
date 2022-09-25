c7fc73419e1c1768aa24179f554f4bf524661df9
Gabriele Andolina <gabe.andolina@gmail.com>
2022-09-25 11:24:17 +0000

    Update food views in cart to include specific messages

--- cart/views.py
+++ cart/views.py
@@ -30,14 +30,17 @@ def add_wine(request, wine_id):
 def add_food(request, food_id):
     """ A view to add a quantity of the selected food to the shopping cart. """
 
+    food = get_object_or_404(Food, pk=food_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if food_id in list(cart.keys()):
         cart[food_id] += quantity
+        messages.success(request, f'Updated {food.name} quantity to {cart[food_id]}.')
     else:
         cart[food_id] = quantity
+        messages.success(request, f'Added {food.name} to the cart. Enjoy!')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
@@ -67,11 +70,13 @@ def adjust_food_quantity(request, food_id):
     within the shopping cart.
     """
 
+    food = get_object_or_404(Food, pk=food_id)
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
     if quantity > 0:
         cart[food_id] = quantity
+        messages.success(request, f'Updated {food.name} quantity to {cart[food_id]}.')
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
@@ -97,10 +102,13 @@ def remove_food(request, food_id):
     """ A view to remove the selected food from the shopping cart. """
 
     try:
+        food = get_object_or_404(Food, pk=food_id)
         cart = request.session.get('cart', {})
         cart.pop(food_id)
+        messages.success(request, f'Removed {food.name} from your cart.')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
     except Exception as e:
+        messages.error(request, f'Oops! An error occurred while removing the item: {e}')
         return HttpResponse(status=500)
