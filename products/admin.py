ae8d367165b56c8182483df2d54a79a92e1ba8d1
Gabriele Andolina <gabe.andolina@gmail.com>
2022-10-07 11:50:13 +0000

    Update products/admin.py after Wine model deletion

--- products/admin.py
+++ products/admin.py
@@ -1,24 +1,11 @@
 from django.contrib import admin
-from .models import Category, Wine, Food, ExperienceType
+from .models import Category, Food
 
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
         'friendly_name',
     )
-
-
-class WineAdmin(admin.ModelAdmin):
-    list_display = (
-        'name',
-        'vintage',
-        'price',
-        'image',
-    )
-
-    ordering = ('name',)
-
-    prepopulated_fields = {"slug": ("name",)}
 
 
 class FoodAdmin(admin.ModelAdmin):
@@ -31,22 +18,6 @@ class FoodAdmin(admin.ModelAdmin):
 
     ordering = ('name',)
 
-    prepopulated_fields = {"slug": ("name",)}
-
-
-class ExperienceTypeAdmin(admin.ModelAdmin):
-    list_display = (
-        'type',
-        'name',
-        'duration',
-        'price',
-        'image',
-    )
-
-    ordering = ('name',)
-
 
 admin.site.register(Category, CategoryAdmin)
-admin.site.register(Wine, WineAdmin)
 admin.site.register(Food, FoodAdmin)
-admin.site.register(ExperienceType, ExperienceTypeAdmin)
