2cdea980cea2a68f269f27c94850f5b5e053e997
Gabriele Andolina <gabe.andolina@gmail.com>
2022-10-07 11:49:03 +0000

    Remove Wine model and update Food model

--- products/models.py
+++ products/models.py
@@ -16,34 +16,10 @@ class Category(models.Model):
         return self.friendly_name
 
 
-class Wine(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    product_type = models.CharField(max_length=255, default="Wine")
-    name = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255, default="wine")
-    image = models.ImageField(null=True, blank=True)
-    description = models.TextField()
-    grape_variety = models.CharField(max_length=255, null=True, blank=True)
-    vintage = models.CharField(max_length=255, null=True, blank=True)
-    certification = models.CharField(max_length=255, null=True, blank=True)
-    colour = models.CharField(max_length=255, null=True, blank=True)
-    type = models.CharField(max_length=255, null=True, blank=True)
-    alcohol_content = models.CharField(max_length=255, null=True, blank=True)
-    format = models.CharField(max_length=255, null=True, blank=True)
-    place_of_origin = models.CharField(max_length=255, null=True, blank=True)
-    vegan_ok = models.ImageField(null=True, blank=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2, null=False)
-
-    def __str__(self):
-        return self.name
-
-
 class Food(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    product_type = models.CharField(max_length=255, default="Food")
     type = models.CharField(max_length=255, null=True, blank=True)
     name = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255, default="food")
     image = models.ImageField(null=True, blank=True)
     description = models.TextField()
     ingredients = models.CharField(max_length=255, null=True, blank=True)
@@ -52,23 +28,6 @@ class Food(models.Model):
     place_of_origin = models.CharField(max_length=255, null=True, blank=True)
     vegan_ok = models.ImageField(null=True, blank=True)
     shelf_life = models.CharField(max_length=255, null=True, blank=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-
-    def __str__(self):
-        return self.name
-
-
-class ExperienceType(models.Model):
-
-    class Meta:
-        verbose_name = 'Experience Type'
-
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    type = models.CharField(max_length=255, null=True, blank=True)
-    name = models.CharField(max_length=255)
-    image = models.ImageField(null=True, blank=True)
-    description = models.TextField()
-    duration = models.CharField(max_length=255, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
