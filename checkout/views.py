e47dbfe5ce4f68f479646db804617cf8e75d48ef
Gabriele Andolina <gabe.andolina@gmail.com>
2022-11-02 11:21:15 +0000

    Add Stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LqNy2I5Gia0Sdxx6UD8Es9tseJ5GphxviO8PPDdGEO1ZFOwUBGxeIv2nLFbOqmEYwaSfPjjCTGCy6SdACFAADBz00m6ZWws6X',
+        'client_secret': 'test_value',
     }
 
     return render(request, template, context)
