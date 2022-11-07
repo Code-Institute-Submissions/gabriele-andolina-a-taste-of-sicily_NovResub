6f8b7b375fd47170653551a482e734483934721e
Gabriele Andolina <gabe.andolina@gmail.com>
2022-11-07 17:57:50 +0000

    Deployment pt.3 - Remove secret key and set debug

--- a_taste_of_sicily/settings.py
+++ a_taste_of_sicily/settings.py
@@ -11,10 +11,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z@l2r@$*10=g&-ko^)qd-!*!gwxi=r-mi_(ybupk2sw57#1mdl'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['a-taste-of-sicily.herokuapp.com', 'localhost']
 
