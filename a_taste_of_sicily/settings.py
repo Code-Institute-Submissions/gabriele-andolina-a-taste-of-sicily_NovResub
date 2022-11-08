352b4e2d72a07829f0e66a8cf0dd6d1fbb6db337
Gabriele Andolina <gabe.andolina@gmail.com>
2022-11-08 12:08:28 +0000

    Update AWS_S3_REGION_NAME

--- a_taste_of_sicily/settings.py
+++ a_taste_of_sicily/settings.py
@@ -174,10 +174,10 @@ if 'USE_AWS' in os.environ:
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
-    
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'a-taste-of-sicily'
-    AWS_S3_REGION_NAME = 'eu-south-1'
+    AWS_S3_REGION_NAME = 'UE (Milano) eu-south-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
