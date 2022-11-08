b3364d7e552ad89761b46a33df864fba154cbe78
Gabriele Andolina <gabe.andolina@gmail.com>
2022-11-08 16:43:34 +0000

    Create experiences view and template

--- experiences/views.py
+++ experiences/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Experience
 
-# Create your views here.
+
+def view_experiences(request):
+    """
+    A view to display the company's 
+    on-the-premises experiences.
+    """
+
+    experiences = Experience.objects.all()
+
+    context = {
+        'experiences': experiences
+    }
+
+    return render(request, 'experiences/experiences.html', context)
