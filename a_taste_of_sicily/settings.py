4d13ba5020c60f2d7605cf39ab599c6539088ba9
Gabriele Andolina <gabe.andolina@gmail.com>
2022-11-08 12:10:12 +0000

    Revert AWS_S3_REGION_NAME to 'eu-south-1'

--- a_taste_of_sicily/settings.py
+++ a_taste_of_sicily/settings.py
@@ -177,7 +177,7 @@ if 'USE_AWS' in os.environ:
 
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'a-taste-of-sicily'
-    AWS_S3_REGION_NAME = 'UE (Milano) eu-south-1'
+    AWS_S3_REGION_NAME = 'eu-south-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
