829000bcada6730c308143ab1f022f4dcc7fd265
Gabriele Andolina <gabe.andolina@gmail.com>
2022-11-08 17:47:03 +0000

    Add view and urls to view experience details

--- experiences/urls.py
+++ experiences/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.view_experiences, name='all_experiences'),
+    path('experience/<int:experience_id>/', views.view_experience_details, name='experience_details'),
 ]
--- experiences/views.py
+++ experiences/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Experience
 
 
@@ -15,3 +15,17 @@ def view_experiences(request):
     }
 
     return render(request, 'experiences/experiences.html', context)
+
+
+def view_experience_details(request, experience_id):
+    """
+    A view to display full details for each experience.
+    """
+
+    experience = get_object_or_404(Experience, pk=experience_id)
+
+    context = {
+        'experience': experience
+    }
+
+    return render(request, 'experiences/experience_details.html', context)
