97962e47aabda4678235507bbf300a898083f00c
Gabriele Andolina <gabe.andolina@gmail.com>
2022-11-08 16:04:14 +0000

    Create Experience model and run migrations

--- experiences/models.py
+++ experiences/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Experience(models.Model):
+    type = models.CharField(max_length=255, null=True, blank=True)
+    name = models.CharField(max_length=255)
+    image = models.ImageField(null=True, blank=True)
+    description = models.TextField()
+    duration = models.IntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
