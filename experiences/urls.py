e3bf79cb6469d01f9077e1ba64926d3365a9fa5a
Gabriele Andolina <gabe.andolina@gmail.com>
2022-11-09 13:19:38 +0000

    Create view and urls to add an experience item

--- experiences/urls.py
+++ experiences/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.view_experiences, name='all_experiences'),
     path('experience/<int:experience_id>/', views.view_experience_details, name='experience_details'),
+    path('add/', views.add_experience, name='add_experience'),
 ]
--- experiences/views.py
+++ experiences/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .models import Experience
+from .forms import ExperienceForm
 
 
 def view_experiences(request):
@@ -29,3 +32,29 @@ def view_experience_details(request, experience_id):
     }
 
     return render(request, 'experiences/experience_details.html', context)
+
+
+@login_required
+def add_experience(request):
+    """ A view to add an experience item to the website """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = ExperienceForm(request.POST, request.FILES)
+        if form.is_valid():
+            experience = form.save()
+            messages.success(request, f'Well done! Your new experience "{ experience.name }" has been added.')
+            return redirect(reverse('experience_details', args=[experience.id]))
+        else:
+            messages.error(request, 'Oops! Something went wrong. Please ensure the form is valid.')
+    else:
+        form = ExperienceForm()
+
+    template = 'experiences/add_experience.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
