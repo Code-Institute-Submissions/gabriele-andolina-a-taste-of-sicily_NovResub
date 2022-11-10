c51274a2de453585d204eb449078f5cbebe05845
Gabriele Andolina <gabe.andolina@gmail.com>
2022-11-10 09:18:18 +0000

    Add pairings field to Food model and reduce Pairing model's fields

--- products/models.py
+++ products/models.py
@@ -16,6 +16,13 @@ class Category(models.Model):
         return self.friendly_name
 
 
+class Pairing(models.Model):
+    name = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Food(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     type = models.CharField(max_length=255, null=True, blank=True)
@@ -28,16 +35,8 @@ class Food(models.Model):
     place_of_origin = models.CharField(max_length=255, null=True, blank=True)
     vegan_ok = models.ImageField(null=True, blank=True)
     shelf_life = models.CharField(max_length=255, null=True, blank=True)
+    pairings = models.ManyToManyField(Pairing, related_name='pairings')
     price = models.DecimalField(max_digits=6, decimal_places=2)
-
-    def __str__(self):
-        return self.name
-
-
-class Pairing(models.Model):
-    main = models.ForeignKey('Food', null=True, blank=True, on_delete=models.SET_NULL)
-    wine = models.CharField(max_length=255, null=True, blank=True)
-    recipes = models.TextField()
 
     def __str__(self):
         return self.name
