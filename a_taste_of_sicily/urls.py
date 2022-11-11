99851772d4bb8bc351aefb4328d6c1b09fc2591b
Gabriele Andolina <gabe.andolina@gmail.com>
2022-11-11 00:51:19 +0000

    Create view, url and template for 404 page

--- a_taste_of_sicily/urls.py
+++ a_taste_of_sicily/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +30,5 @@ urlpatterns = [
     path('profiles/', include('profiles.urls')),
     path('experiences/', include('experiences.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'a_taste_of_sicily.views.handler404'
